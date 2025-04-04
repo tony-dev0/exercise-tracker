941961ba9c51b7ccf99118fc5babd370923a87c6
tony-dev0 <rollandjp34@gmail.com>
2025-04-04 07:50:25 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,116 +1,97 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Users = require("./models/Usersmodel");
 const Exercises = require("./models/Usersmodel");
+require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URI);
 
-app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use("/public", express.static(process.cwd() + "/public"));
-
+app.use(cors());
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-
-app.post("/api/users", function (req, res) {
-  if (req.body.username === "") {
-    return res.json({ error: "username is required" });
-  }
-
-  let username = req.body.username;
-  let _id = "";
-
-  Users.findOne({ username: username }, function (err, data) {
-    if (!err && data === null) {
-      let newUser = new Users({
-        username: username,
-      });
-
-      newUser.save(function (err, data) {
-        if (!err) {
-          _id = data["_id"];
-
-          return res.json({
-            _id: _id,
-            username: username,
-          });
-        }
-      });
-    } else {
-      return res.json({ error: "username already exists" });
-    }
-  });
+app.post("/api/users", async function (req, res) {
+  const inputUsername = req.body.username;
+  // Create a new user
+  let newUser = new Users({ username: inputUsername });
+  // save the user to the database
+  const savedUser = await newUser.save();
+  savedUser
+    .then(() => {
+      res.json({ username: user.username, _id: user._id });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({ message: "User creation failed!" });
+    });
 });
-
+// Get all the users
 app.get("/api/users", function (req, res) {
   Users.find({}, function (err, data) {
     if (!err) return res.json(data);
   });
 });
-
+// post a new exercise
 app.post("/api/users/:_id/exercises", function (req, res) {
-  if (req.params._id === "0") {
-    return res.json({ error: "_id is required" });
+  var userId = req.params._id;
+  var description = req.body.description;
+  var duration = req.body.duration;
+  var date = req.body.date;
+
+  // Check for date
+  if (!date) {
+    date = new Date().toISOString().substring(0, 10);
   }
 
-  if (req.body.description === "") {
-    return res.json({ error: "description is required" });
-  }
+  console.log(
+    "looking for user with id [".toLocaleUpperCase() + userId + "] ..."
+  );
 
-  if (req.body.duration === "") {
-    return res.json({ error: "duration is required" });
-  }
-
-  let userId = req.params._id;
-  let description = req.body.description;
-  let duration = parseInt(req.body.duration);
-  let date = req.body.date !== undefined ? new Date(req.body.date) : new Date();
-
-  if (isNaN(duration)) {
-    return res.json({ error: "duration is not a number" });
-  }
-
-  if (date == "Invalid Date") {
-    return res.json({ error: "date is invalid" });
-  }
-
-  Users.findById(userId, function (err, data) {
-    if (!err && data !== null) {
-      let newExercise = new Exercises({
-        userId: userId,
-        description: description,
-        duration: duration,
-        date: date,
-      });
-
-      newExercise.save(function (err2, data2) {
-        if (!err2) {
-          return res.json({
-            _id: data["_id"],
-            username: data["username"],
-            description: data2["description"],
-            duration: data2["duration"],
-            date: new Date(data2["date"]).toDateString(),
-          });
-        }
-      });
-    } else {
-      return res.json({ error: "user not found" });
+  // Find the user
+  Users.findById(userId, async (err, userInDb) => {
+    if (err) {
+      console.error(err);
+      res.json({ message: "There are no users with that ID in the database!" });
     }
+    // Create new exercise
+    let newExercise = new Exercises({
+      userId: userInDb._id,
+      username: userInDb.username,
+      description: description,
+      duration: parseInt(duration),
+      date: date,
+    });
+
+    const savedExercise = await newExercise.save();
+    savedExercise
+      .then((exercise) => {
+        console.log("exercise saved to DB");
+        res.json({
+          username: userInDb.username,
+          description: exercise.description,
+          duration: exercise.duration,
+          date: new Date(exercise.date).toDateString(),
+          _id: userInDb._id,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.json({ message: "Exercise creation failed!" });
+      });
   });
 });
-//
+
+// Get all the exercises of a user
 app.get("/api/users/:_id/exercises", function (req, res) {
   res.redirect("/api/users/" + req.params._id + "/logs");
 });
-
+// Get all the logs of a user
 app.get("/api/users/:_id/logs", async function (req, res) {
   const userId = req.params._id;
   const from = req.query.from || new Date(0).toISOString().substring(0, 10);
@@ -120,14 +101,10 @@ app.get("/api/users/:_id/logs", async function (req, res) {
 
   console.log("get user log");
 
-  //? Find the user
+  // Find the user
   let user = await Users.findById(userId).exec();
 
-  console.log(
-    "looking for exercises with id [".toLocaleUpperCase() + userId + "] ..."
-  );
-
-  //? Find the exercises
+  // Find the exercises
   let exercises = await Exercises.find({
     userId: userId,
     date: { $gte: from, $lte: to },
